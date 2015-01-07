71c480c2afd1fb63401fe315c1ab43870af1f49c
MalinaD <malina.demirova@gmail.com>
2015-01-07 19:26:19 +0200

    Added changes to the home view and ads view

--- AdsOnSPA/AdsOnSPA/scripts/controllers/HomeController.js
+++ AdsOnSPA/AdsOnSPA/scripts/controllers/HomeController.js
@@ -2,6 +2,14 @@
 
 app.controller('HomeController',
     function ($scope, $rootScope, adsService, notifyService, pageSize) {
-        // TODO
+        adsService.getAds(
+            null,
+            function success(data) {
+                //TODO
+            },
+            function error(err) {
+                notifyService.showError("Cannot load ads", err);
+            }
+            );
     }
 );
