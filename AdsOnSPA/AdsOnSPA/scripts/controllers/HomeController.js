65e12ebdc46bb5631735cb618327c2ca127ea9b2
MalinaD <malina.demirova@gmail.com>
2015-01-08 00:31:28 +0200

    Small changes

--- AdsOnSPA/AdsOnSPA/scripts/controllers/HomeController.js
+++ AdsOnSPA/AdsOnSPA/scripts/controllers/HomeController.js
@@ -4,8 +4,8 @@ app.controller('HomeController',
     function ($scope, $rootScope, adsService, notifyService, pageSize) {
         adsService.getAds(
             null,
-            function success(data) {
-                //TODO
+            function success(data,status, headers, config) {
+               
             },
             function error(err) {
                 notifyService.showError("Cannot load ads", err);
