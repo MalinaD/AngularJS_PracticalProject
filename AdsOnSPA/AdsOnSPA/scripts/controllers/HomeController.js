9c9256c22109281d8203b66625ea014933b6010d
MalinaD <malina.demirova@gmail.com>
2015-01-08 14:45:48 +0200

    Modify the header, change the leftsidebar

--- AdsOnSPA/AdsOnSPA/scripts/controllers/HomeController.js
+++ AdsOnSPA/AdsOnSPA/scripts/controllers/HomeController.js
@@ -1,15 +1,15 @@
 ﻿'use strict';
 
-app.controller('HomeController',
+app.controller('HomeController',['$scope',
     function ($scope, $rootScope, adsService, notifyService, pageSize) {
         adsService.getAds(
             null,
             function success(data,status, headers, config) {
-               
+                $scope.pageTitle = ' Home';
             },
             function error(err) {
                 notifyService.showError("Cannot load ads", err);
             }
             );
     }
-);
+]);
