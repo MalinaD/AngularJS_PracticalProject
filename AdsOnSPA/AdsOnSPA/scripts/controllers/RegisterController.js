f34192e4b822fb60e4bc9e2a812ebb7b3e886f26
MalinaD <malina.demirova@gmail.com>
2015-01-09 12:42:47 +0200

    Testing Register function

--- AdsOnSPA/AdsOnSPA/scripts/controllers/RegisterController.js
+++ AdsOnSPA/AdsOnSPA/scripts/controllers/RegisterController.js
@@ -1,12 +1,15 @@
 ﻿'use strict';
 
 app.controller('RegisterController',
-    function ($scope, $rootScope, $location, authService, notifyService) {
+    function ($scope, $rootScope, $location,townsServices, authService, notifyService) {
+        $scope.userData = { townId: null };
+        $scope.towns = townsServices.getTowns();
+
         $scope.register = function (userData) {
             authService.register(userData,
                 function success() {
                     notifyService.showInfo("Registration successful");
-                    $location.path("/");
+                    $location.path("/login");
                 },
                 function error(err) {
                     notifyService.showError("User registration failed", err);
---
+++ AdsOnSPA/AdsOnSPA/scripts/services/notifyService.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+
+app.factory('notifyService',
+function () {
+    //TODO
+});
